f5181dca572884f7e757e0b864a2a7f96d04c289
DELL <anyguess24@gmail.com>
2025-02-01 17:51:21 +0530

    I have made changes in the coding of Program.cs files and add few more files also thanks

--- AuthECAPI/Program.cs
+++ AuthECAPI/Program.cs
@@ -1,3 +1,5 @@
+using AuthECAPI.Controllers;
+using AuthECAPI.Extensions;
 using AuthECAPI.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -14,128 +16,26 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//Services from Identity Core
-builder.Services.AddIdentityApiEndpoints<AppUser>().AddEntityFrameworkStores< AppDbContext>();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;  
-    options.Password.RequireUppercase = false; 
-    options.User.RequireUniqueEmail = true;    
-});
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
-
-builder.Services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme =
-                    x.DefaultChallengeScheme =
-                    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;   
-                 }).AddJwtBearer(y =>
-                 {
-                     y.SaveToken = false;
-                     y.TokenValidationParameters = new TokenValidationParameters
-                     {
-                         ValidateIssuerSigningKey = true,
-                         IssuerSigningKey = new SymmetricSecurityKey(
-                             Encoding.UTF8.GetBytes(
-                                 builder.Configuration["AppSettings:JWTSecret"]!
-                                 ))
-                     };
-                 });
+builder.Services.AddSwaggerExplorer()
+                .InjectDbContext(builder.Configuration)
+                .AddAppConfig(builder.Configuration)
+                .AddIdentityHandlersAndStores()
+                .ConfigureIdentityOptions()      
+                .AddIdentityAuth(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-#region Config. CORS
-app.UseCors(options => options.WithOrigins("http://localhost:4200")
-                                .AllowAnyMethod()
-                                .AllowAnyHeader());
-#endregion
-app.UseAuthentication();    
-app.UseAuthorization();
+app.ConfigureSwaggerExplorer()
+   .ConfigureCORS(builder.Configuration)
+   .AddIdentityAuthMiddlewares();
 
 app.MapControllers();
-
-app.MapGroup("/api").MapIdentityApi<AppUser>();
-
-app.MapPost("/api/signup", async (
-    UserManager<AppUser> userManager,
-    [FromBody] UserRegistrationModel userRegistrationModel
-    ) =>
-    {
-        AppUser user = new AppUser()
-        {
-            UserName = userRegistrationModel.Email,
-            Email = userRegistrationModel.Email,
-            FullName = userRegistrationModel.FullName,
-        };
-        var result = await userManager.CreateAsync(user, userRegistrationModel.Password);
-    
-        if (result.Succeeded)
-            return Results.Ok(result);
-        else
-            return Results.BadRequest(result);
-    });
-
-app.MapPost("/api/signin", async (
-    UserManager<AppUser> userManager,
-    [FromBody] LogiModel loginModel) =>
-{
-    var user = await userManager.FindByEmailAsync(loginModel.Email);
-    if (user != null && await userManager.CheckPasswordAsync(user, loginModel.Password))
-    {
-        var signInKey = new SymmetricSecurityKey(
-                             Encoding.UTF8.GetBytes(
-                                 builder.Configuration["AppSettings:JWTSecret"]!
-                                 ));
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new Claim("UserID",user.Id.ToString())
-            }),
-            Expires = DateTime.UtcNow.AddDays(10),
-            SigningCredentials = new SigningCredentials(
-                signInKey,
-                SecurityAlgorithms.HmacSha256Signature
-            )     
-        };
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
-        var token = tokenHandler.WriteToken(securityToken);
-        return Results.Ok(new {token});
-    }
-    else
-        return Results.BadRequest(new { message = "Username or password is incorrect." });
-});
+app.MapGroup("/api")
+   .MapIdentityApi<AppUser>();
+app.MapGroup("/api")
+   .MapIdentityUserEndpoints(); 
 
 app.Run();
 
-public class UserRegistrationModel
-{
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string FullName { get; set; }
-}
-
-public class LogiModel
-{
-    public string Email { get; set; }
-    public string Password { get; set; }
-    
-}
 
